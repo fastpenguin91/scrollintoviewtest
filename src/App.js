29cc8db1c418e5f70fd76d2a483cbfcfc04647f0
John Curry <fastpenguin91@gmail.com>
2021-03-09 13:12:45 -0800

    works with separate functions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import React, { useRef, forwardRef } from "react";
 import CalendarList from "./CalendarList";
 
-const Article = forwardRef(({ onBackClick }, ref) => {
+const Article = forwardRef(({ onBackClick, language }, ref) => {
   // multiple of these
   // const titleRef = useRef();
 
@@ -28,7 +28,7 @@ const Article = forwardRef(({ onBackClick }, ref) => {
       <p>...</p>
       <p>...</p>
       <p>...</p>
-      <h1 ref={ref}>A React article for Latin Readers</h1>
+      <h1 ref={ref}>A React article for {language} Readers</h1>
       <p>...</p>
       <p>...</p>
       <p>...</p>
@@ -60,27 +60,60 @@ const Article = forwardRef(({ onBackClick }, ref) => {
       <p>...</p>
       <p>...</p>
       <p>...</p>
-      <button onClick={onBackClick}>Back to top</button>
     </article>
   );
 });
 
-const AnotherComponent = () => {
+const AnotherComponent = ({
+  englishArticleRef,
+  latinArticleRef,
+  handleEnglishBackClick,
+  handleLatinBackClick,
+}) => {
   // one of these
-  const articleRef = useRef();
+  // const articleRef = useRef();
 
-  function handleBackClick() {
-    articleRef.current.scrollIntoView({ behavior: "smooth" });
-  }
+  // function handleBackClick() {
+  //   articleRef.current.scrollIntoView({ behavior: "smooth" });
+  // }
 
-  return <Article ref={articleRef} onBackClick={handleBackClick} />;
+  return (
+    <div>
+      <Article
+        ref={latinArticleRef}
+        language={"Latin"}
+        onBackClick={handleLatinBackClick}
+      />
+      <Article
+        ref={englishArticleRef}
+        language={"English"}
+        onBackClick={handleEnglishBackClick}
+      />
+    </div>
+  );
 };
 
 function App() {
+  const englishArticleRef = useRef();
+  const latinArticleRef = useRef();
+  console.log("still works???");
+
+  function handleEnglishBackClick() {
+    console.log("selectedRef in handleBackClick: ");
+    englishArticleRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  function handleLatinBackClick() {
+    console.log("selectedRef in handleBackClick: ");
+    latinArticleRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <div className="App">
       <header>
-        Header area <button>other back to top</button>
+        Header area{" "}
+        <button onClick={handleLatinBackClick}>Latin Article</button>
+        <button onClick={handleEnglishBackClick}>English Article</button>
       </header>
       <div
         style={{
@@ -90,7 +123,12 @@ function App() {
           background: "hotpink",
         }}
       >
-        <AnotherComponent />
+        <AnotherComponent
+          englishArticleRef={englishArticleRef}
+          latinArticleRef={latinArticleRef}
+          handleEnglishBackClick={handleEnglishBackClick}
+          handleLatinBackClick={handleLatinBackClick}
+        />
       </div>
       {/* <CalendarList /> */}
     </div>
